451e47653181720d2e04c76c6a6fd61473801363
Ida Kristine Sandford Meitil <ida.meitil@gmail.com>
2022-05-30 13:08:53 +0200

    added O-antigen ligases

--- src/data-collection-and-preprocessing/retrieve-wzx-wzz-and-waal.py
+++ src/data-collection-and-preprocessing/retrieve-wzx-wzz-and-waal.py
@@ -12,14 +12,18 @@ def get_cds_feature_with_qualifier_value(seq_record, name, value):
     # Could not find it
     return None
 
+
 gb_file = 'data/selected-OAGCs.gb'
 
 names_wzz = [("gene", "wzz"), ("gene", "wzz (rol)"), ("gene", "wzzE"), ("gene", "wzzB"), ("gene", "wzz(fepE)"), ("product", "Wzz")]
 names_wzx = [("gene", "wzx"), ("gene", "WzxC"), ("gene", "WzxE")]
+names_waal = [("gene", "waaL"), ("product", "O-antigen ligase")]
 wzz_data = []
 wzx_data = []
+waal_data = []
 wzz_accessions_done = []
 wzx_accessions_done = []
+waal_accessions_done = []
 for gb_record in SeqIO.parse(open(gb_file,"r"), "genbank") :
     dna_accession = gb_record.id
     organism = gb_record.annotations['organism']
@@ -41,8 +45,18 @@ for gb_record in SeqIO.parse(open(gb_file,"r"), "genbank") :
                 wzx_seq = feature.qualifiers.get('translation')[0]
                 wzx_data.append((wzx_accession, organism, dna_accession, wzx_seq))
                 wzx_accessions_done.append(wzx_accession)
+    # WaaL
+    for name, value in names_waal:
+        feature = get_cds_feature_with_qualifier_value(gb_record, name, value)
+        if feature is not None:
+            waal_accession = feature.qualifiers.get('protein_id')[0]
+            if waal_accession not in waal_accessions_done:
+                waal_seq = feature.qualifiers.get('translation')[0]
+                waal_data.append((waal_accession, organism, dna_accession, waal_seq))
+                waal_accessions_done.append(waal_accession)
 wzz_df = pd.DataFrame(wzz_data, columns = ['protein_accession', 'organism', 'dna_accession', 'seq'])
 wzx_df = pd.DataFrame(wzx_data, columns = ['protein_accession', 'organism', 'dna_accession', 'seq'])
+waal_df = pd.DataFrame(waal_data, columns = ['protein_accession', 'organism', 'dna_accession', 'seq'])
 
 ### Merge with wzy table ###
 
@@ -50,16 +64,21 @@ wzx_df = pd.DataFrame(wzx_data, columns = ['protein_accession', 'organism', 'dna
 wzy_df = pd.read_csv("data/wzy/wzy.tsv", sep = '\t', dtype=object)
 wzy_columns = wzy_df.columns.drop(['WzyE', 'comment']).tolist()
 # Remove ECA rows
-wzy_df = wzy_df[wzy_df.WzyE == '0'] 
+wzy_df = wzy_df[wzy_df.WzyE == '0']
+# Remove WaaL rows
+wzy_df = wzy_df[wzy_df.WaaL == '0']
 # Remove columns
 wzy_df.drop(['protein_accession', 'seq'], axis=1, inplace=True)
 # Merge
 wzz_merged_df = pd.merge(wzz_df, wzy_df, on='dna_accession', how='left')
 wzx_merged_df = pd.merge(wzx_df, wzy_df, on='dna_accession', how='left')
+waal_merged_df = pd.merge(waal_df, wzy_df, on='dna_accession', how='left')
 
 ### Write to file ###
 wzz_final_df = wzz_merged_df.loc[:,wzy_columns]
 wzx_final_df = wzx_merged_df.loc[:,wzy_columns]
+waal_final_df = waal_merged_df.loc[:,wzy_columns]
 
 wzz_final_df.to_csv("data/wzz/wzz.tsv", sep='\t', index=False)
 wzx_final_df.to_csv("data/wzx/wzx.tsv", sep='\t', index=False)
+waal_final_df.to_csv("data/waal/waal.tsv", sep='\t', index=False)
