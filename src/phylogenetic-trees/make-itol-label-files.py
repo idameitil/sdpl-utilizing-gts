6fa3ebadb7b5008ba53b5a29da7cbf18c9af4b53
Ida Kristine Sandford Meitil <ida.meitil@gmail.com>
2022-06-08 11:13:45 +0200

    made iTOL image label file

--- src/phylogenetic-trees/make-itol-label-files.py
+++ src/phylogenetic-trees/make-itol-label-files.py
@@ -4,7 +4,7 @@ import random
 
 seed_df = pd.read_csv("data/wzy/wzy.tsv", sep='\t')
 
-# Make taxonomy label files
+# Taxonomy label files
 wanted_ranks = ['phylum', 'class', 'order', 'family', 'genus', 'species']
 for rank in wanted_ranks:
     outfilename = f"data/wzy/phylogenetic-trees/itol-label-files/{rank}.txt"
@@ -20,14 +20,13 @@ for rank in wanted_ranks:
                 tax2color[tax] = color
             file.write(f"{acc},{tax2color[tax]},{tax}\n")
 
-
-outfilename = f"data/wzy/phylogenetic-trees/itol-label-files/polysaccharide_type.txt"
+# Polysaccharide type
+outfilename = f"data/wzy/phylogenetic-trees/itol-label-files/polysaccharide-type.txt"
 with open(outfilename, "w") as file:
     header = f"DATASET_COLORSTRIP\nSEPARATOR COMMA\nDATASET_LABEL,Polysaccharide type\nCOLOR\t#ff0000\nDATA\n"
     file.write(header)
     for index, row in seed_df.iterrows():
         colors = {'ECA Wzy':'green', 'CPS Wzy':'red', 'O-antigen Wzy':'blue'}
-        print(row.WaaL)
         if row.WzyE == 1:
             type = 'ECA Wzy'
         elif row.CPS == 1:
@@ -35,3 +34,12 @@ with open(outfilename, "w") as file:
         else:
             type = 'O-antigen Wzy'
         file.write(f"{row['protein_accession']},{colors[type]},{type}\n")
+
+# Image label file
+outfilename = f"data/wzy/phylogenetic-trees/itol-label-files/sugar-images.txt"
+with open(outfilename, "w") as file:
+    header = "DATASET_IMAGE\nSEPARATOR TAB\nDATASET_LABEL\timage\nCOLOR,#ff0000\nMARGIN\t0\nSHOW_INTERNAL\t"\
+                "t0\nIMAGE_ROTATION\t0\nIMAGE_SHIFT_V\t0\nIMAGE_SHIFT_H\t0\nSIZE_FACTOR	1\nDATA\n"
+    file.write(header)
+    for index, row in seed_df.iterrows():
+        file.write(f"{row.protein_accession}\t-1\t1\t0\t0\t0\t{row.csdb_image_path}\n")
