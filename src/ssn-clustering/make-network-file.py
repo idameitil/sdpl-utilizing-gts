5f4077730d0de40f76993771d169c9ee0a526c58
Ida Kristine Sandford Meitil <ida.meitil@gmail.com>
2022-07-04 10:29:27 +0200

    cleaned up make-network file

--- src/ssn-clustering/make-network-file.py
+++ src/ssn-clustering/make-network-file.py
@@ -1,53 +1,57 @@
 import os
-import sys
+import multiprocessing as mp
 
-def parse_blast_file(blast_file, outfile):
+run_dir = f"data/wzy/all-vs-all-blast/run"
+
+def parse_blast_file(blast_filename):
     """Parses a blast file and appends protein pairs to network list"""
-
+    
+    print(f"Parsing {chunk}")
     done = []
     header_hit_list = False
     hit_list_section = False
-    infile = open(blast_file)
-    for line in infile:
-        # Query accession line
-        if line.startswith("Query="):
-            query_accession = line.strip().split(' ')[1]
-            done.append(query_accession)
-        # Two lines before hit list section
-        elif line.startswith('Sequences producing significant alignments:'):
-            header_hit_list = True
-        # One line before hit list section
-        elif header_hit_list:
-            if line.strip() == '':
-                header_hit_list = False
-                hit_list_section = True
-        # Hit list section
-        elif hit_list_section:
-            # End of hit list section
-            if line.strip() == '':
-                hit_list_section = False
-            # Hits
-            else:
-                splitted_line = line.split()
-                target_accession = splitted_line[0].split(' ')[0]
-                score = splitted_line[-2]
-                e_value = splitted_line[-1]
-                if target_accession not in done:
-                    outfile.write(f"{query_accession}\t{target_accession}\t{score}\t{e_value}\n")
-    infile.close()
+    results = []
+    with open(blast_filename, 'r') as infile:
+        for line in infile:
+            # Query accession line
+            if line.startswith("Query="):
+                query_accession = line.strip().split(' ')[1]
+                done.append(query_accession)
+            # Two lines before hit list section
+            elif line.startswith('Sequences producing significant alignments:'):
+                header_hit_list = True
+            # One line before hit list section
+            elif header_hit_list:
+                if line.strip() == '':
+                    header_hit_list = False
+                    hit_list_section = True
+            # Hit list section
+            elif hit_list_section:
+                # End of hit list section
+                if line.strip() == '':
+                    hit_list_section = False
+                # Hits
+                else:
+                    splitted_line = line.split()
+                    target_accession = splitted_line[0].split(' ')[0]
+                    score = splitted_line[-2]
+                    e_value = splitted_line[-1]
+                    if target_accession not in done:
+                        results.append({'query_accession': query_accession, 'target_accession': target_accession, 'score': score, 'e_value': e_value})
+    return results
 
-# Prepare outfile
-outfilename = f"data/wzy/all-vs-all-blast/network"
-outfile = open(outfilename, "w")
-outfile.write("source\ttarget\tscore\tevalue\n")
+if __name__ == '__main__':
 
-# Parse blast files and write to outfile
-run_dir = f"data/wzy/all-vs-all-blast/run"
-chunks = [f for f in os.listdir(run_dir) if not f.startswith('.')]
-count = 0
-for chunk in chunks:
-    count += 1
-    print(f'Parsing blast output for chunk {count} out of {len(chunks)}')
-    blast_file = f"{run_dir}/{chunk}/blast.out"
-    parse_blast_file(blast_file, outfile)
-outfile.close()
+    chunks = [f for f in os.listdir(run_dir) if not f.startswith('.')]
+
+    outfilename = f"data/wzy/all-vs-all-blast/network2"
+    with open(outfilename, 'w') as outfile:
+        outfile.write("source\ttarget\tscore\tevalue\n")
+        count = 0
+        for chunk in chunks:
+            count += 1
+            print(count)
+            blast_filename = f"{run_dir}/{chunk}/blast.out"
+            results = parse_blast_file(blast_filename)
+            for line in results:
+                outfile.write(f"{line['query_accession']}\t{line['target_accession']}\t{line['score']}\t{line['e_value']}\n")
