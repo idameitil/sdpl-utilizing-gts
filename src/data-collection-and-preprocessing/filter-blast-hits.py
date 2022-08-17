eeb19dbaf27c3eb2dc83e81cbb3ff4984b7457d2
Ida Kristine Sandford Meitil <ida.meitil@gmail.com>
2022-08-17 16:34:17 +0200

    common script for filtering blast hits

--- src/data-collection-and-preprocessing/filter-blast-hits.py
+++ src/data-collection-and-preprocessing/filter-blast-hits.py
@@ -3,17 +3,14 @@ import os
 # Length filter
 infile = open('data/wzy/blast/unique-hits.fasta')
 outfile = open('data/wzy/blast/unique-hits-min320max600.fasta', 'w')
-#outfile_tsv = open("data/wzy/blast/unique-hits-min320max600.tsv", 'w')
-#outfile_tsv.write('accession\tsequence\n')
 first = True
 for line in infile:
     # Header
     if line.startswith('>'):
         # Save previous
         if not first:
-            if len(seq) > 320 and len(seq) < 600 and seq.startswith("M"):
+            if len(seq) > 320 and len(seq) < 600:
                 outfile.write(f'>{acc}\n{seq}\n')
-                #outfile_tsv.write(f'{acc}\t{seq}\n')
         acc = line.split(' ')[0][1:]
         seq = ''
         first = False
@@ -23,7 +20,6 @@ for line in infile:
 
 infile.close()
 outfile.close()
-#outfile_tsv.close()
 
 # CD-HIT
 os.system(f'cd-hit -i data/wzy/blast/unique-hits-min320max600.fasta -o data/wzy/blast/unique-hits-min320max600-cdhit99.fasta -c 0.99')
