3b76d515580359027b27761f977db5a573a4e9c7
Ida Kristine Sandford Meitil <ida.meitil@gmail.com>
2022-08-24 09:50:15 +0200

    script for building hmms

--- src/ssn-clustering/build-hmms.py
+++ src/ssn-clustering/build-hmms.py
@@ -5,5 +5,5 @@ timestamp = sys.argv[1]
 clusters = [dir for dir in os.listdir(f"data/wzy/ssn-clusterings/{timestamp}/clusters") if not dir.startswith('.')]
 
 for cluster in clusters:
-    command = f"hmmbuild  -n {cluster} data/wzy/ssn-clusterings/{timestamp}/clusters/{cluster}/cluster.hmm data/wzy/ssn-clusterings/{timestamp}/clusters/{cluster}/sequences.afa"
+    command = f"hmmbuild  -n {cluster} data/wzy/ssn-clusterings/{timestamp}/clusters/{cluster}/{cluster}.hmm data/wzy/ssn-clusterings/{timestamp}/clusters/{cluster}/sequences.afa"
     os.system(command)
