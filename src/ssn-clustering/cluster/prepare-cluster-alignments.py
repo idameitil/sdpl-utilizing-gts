3301dc3da730faddb5dee5a3f384b021ec2f5f5a
Ida Kristine Sandford Meitil <ida.meitil@gmail.com>
2022-09-08 11:34:17 +0200

    changed to not make logo plots

--- src/ssn-clustering/cluster/prepare-cluster-alignments.py
+++ src/ssn-clustering/cluster/prepare-cluster-alignments.py
@@ -21,8 +21,8 @@ for cluster in clusters:
     
     commands = f"""/work3/idamei/bin/muscle5.1.linux_intel64 -{align_method} {hpc_directory}/{cluster}/sequences.fa -output {hpc_directory}/{cluster}/sequences.afa
 /work3/garryg/bioP/bin/malign.py -a {hpc_directory}/{cluster}/sequences.afa -s {hpc_directory}/{cluster}/seeds.txt -l 1000 > {hpc_directory}/{cluster}/sequences.malign
-python2 /work3/idamei/bin/seq2logo-2.1/Seq2Logo.py -f {hpc_directory}/{cluster}/sequences.afa -m 0.0001 -b 0 -o {hpc_directory}/{cluster}/sequences.logo  -p 640x1000 -l 30  -I 1
 /work3/idamei/bin/FastTree {hpc_directory}/{cluster}/sequences.afa > {hpc_directory}/{cluster}/sequences.nwk"""
+# python2 /work3/idamei/bin/seq2logo-2.1/Seq2Logo.py -f {hpc_directory}/{cluster}/sequences.afa -m 0.0001 -b 0 -o {hpc_directory}/{cluster}/sequences.logo  -p 640x1000 -l 30  -I 1
 
     jobscript_name = f"{clusterdir}/{cluster}/jobscript.sh"
     write_jobscript(script_path=jobscript_name, jobname=cluster, no_gb=5, no_hours=10, 
