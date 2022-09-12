c00fe3b0a44a46c2091bae61f1cb3ede98aa3250
Ida Kristine Sandford Meitil <ida.meitil@gmail.com>
2022-09-12 11:25:03 +0200

    renamed prepare-all-vs-all.sh

---
+++ src/all-vs-all-blast/prepare-all-vs-all.sh
@@ -0,0 +1,4 @@
+
+
+scp src/all-vs-all-blast/prepare-all-vs-all.py idamei@transfer.gbar.dtu.dk:/work3/idamei/$2/all-vs-all-blast/
+scp $1 idamei@transfer.gbar.dtu.dk:/work3/idamei/$2/all-vs-all-blast/db/
