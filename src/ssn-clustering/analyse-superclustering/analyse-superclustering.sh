417ff77369bf7b5c2f40b327d65cbcd142394c9d
Ida Kristine Sandford Meitil <ida.meitil@gmail.com>
2022-10-12 09:17:17 +0200

    stereochemistry plot

--- src/ssn-clustering/analyse-superclustering/analyse-superclustering.sh
+++ src/ssn-clustering/analyse-superclustering/analyse-superclustering.sh
@@ -7,5 +7,8 @@ python src/ssn-clustering/analyse-superclustering/make-report-superclusters.py $
 echo "\n*** GENERATING SUMMARY TABLE ***\n"
 python src/ssn-clustering/analyse-superclustering/make-table-superclusters.py $1 $2
 
+echo "\n*** MAKING STEREOCHEMISTRY PLOT ***\n"
+python src/ssn-clustering/analyse-superclustering/plot-stereochemistry.py $1 $2
+
 echo "\n*** BUILDING HMMS ***\n"
 python src/ssn-clustering/analyse-superclustering/build-hmms-superclusters.py $1 $2
---
+++ src/ssn-clustering/analyse-superclustering/plot-stereochemistry.py
@@ -0,0 +1,44 @@
+import sys
+sys.path.append('src/ssn-clustering/analyse-clustering')
+from common import SSNClusterData
+import numpy as np
+import matplotlib.pyplot as plt
+
+ssn_timestamp = sys.argv[1]
+superclustering_timestamp = sys.argv[2]
+
+# Get clustering data
+clustering_data = SSNClusterData(ssn_timestamp, superclustering_timestamp, 
+                                load_clusters=False, calculate_conserved=False, 
+                                get_sugars=False,  get_sugars_superclusters=False)
+superclusters = list(clustering_data.superclusters)
+
+# Prepare plot data
+labels = []
+axial_counts = []
+equatorial_counts = []
+count = 0
+for supercluster in superclusters:
+    labels.append(supercluster['name'])
+    axial_counts.append(supercluster['bond_counts']['axial_count'])
+    equatorial_counts.append(supercluster['bond_counts']['equatorial_count'])
+    count += 1
+    if count == 30:
+        break
+
+# Make plot
+x = np.arange(len(labels))
+width = 0.4
+
+fig, ax = plt.subplots(figsize=(10, 4))
+
+ax.bar(labels, axial_counts, width, label='Axial bonds')
+ax.bar(labels, equatorial_counts, width, bottom=axial_counts,
+       label='Equatorial bonds')
+
+ax.set_ylabel('Count')
+ax.set_xlabel('Supercluster')
+ax.set_title('')
+ax.legend()
+
+plt.savefig(f"data/wzy/ssn-clusterings/{ssn_timestamp}/superclusterings/{superclustering_timestamp}/stereochemistry.png")
