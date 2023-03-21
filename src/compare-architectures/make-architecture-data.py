c6aaa68f1e4e0fbd65707fe22ab9f1bb29aa0576
Ida Kristine Sandford Meitil <ida.meitil@gmail.com>
2023-03-21 15:55:14 +0100

    compare architectures

--- src/compare-architectures/make-architecture-data.py
+++ src/compare-architectures/make-architecture-data.py
@@ -2,8 +2,9 @@ import sys, os
 import pandas as pd
 sys.path.append("src/ssn-clustering")
 from common import read_MSA_file, get_conserved_residues, get_specific_positions_conserved_residues
+import json
 
-threshold = 0.96
+threshold = 0.98
 
 def get_name(entry):
     if entry['family'] == 'X571':
@@ -17,68 +18,68 @@ def get_name(entry):
     return name
 
 # All, with subfamily conservation if available
-entries = [
-           [{'acc': '6bar', 'family': 'X571'},
-           ],
-           [
-           {'acc': 'WP_011517284.1', 'family': 'X615_A'},
-           {'acc': 'CAR61594.1', 'family': 'X615_C'}
-           ],
-           [
-           {'acc': 'ACD37165.1', 'family': 'X614_B'},
-           {'acc': 'AAM27615.1', 'family': 'X614_C'}
-           ],
-           [
-           {'acc': 'CAI34254.1', 'family': 'X609'},
-           {'acc': 'CAI34198.1', 'family': 'X609'},
-           {'acc': 'CAI34217.1', 'family': 'X609'}
-           ],
-           [
-           {'acc': 'CAI34124.1', 'family': 'X607'},
-           ],
-           [
-           {'acc': 'CAI33309.1', 'family': 'X613'},
-           {'acc': 'CAI34369.1', 'family': 'X613'},
-           {'acc': 'AIG62747.1', 'family': 'X613'}
-           ],
-           [
-           {'acc': 'BAQ00795.1', 'family': 'X605_C'},
-           {'acc': 'BAQ01659.1', 'family': 'X605'},
-           {'acc': 'BAQ02088.1', 'family': 'X605_B'},
-           {'acc': 'ACA24754.1', 'family': 'X605_B'}
-           ],
-           [
-           {'acc': 'AHB32215.1', 'family': 'X617_D'},
-           {'acc': 'AHB32861.1', 'family': 'X617_D'},
-           {'acc': 'CAI34008.1', 'family': 'X617_A'},
-           {'acc': 'AHB32334.1', 'family': 'X617'},
-           {'acc': 'ACH97162.1', 'family': 'X617'} 
-           ],
-           [
-           {'acc': 'AHB32411.1', 'family': 'X610'}
-           ],
-           [
-           {'acc': 'BAQ00653.1', 'family': 'X612'},
-           {'acc': 'ADC54950.1', 'family': 'X612_B'},
-           {'acc': 'AAZ85718.1', 'family': 'X612'}
-           ],
-           [
-           {'acc': 'AAT77177.1', 'family': 'X606_B'},
-           {'acc': 'CBN82200.1', 'family': 'X606_A'}
-           ],
-           [
-           {'acc': 'CDF66396.1', 'family': 'X611_B'},
-           {'acc': 'AAA97573.1', 'family': 'X611_A'},
-           {'acc': 'AAC45857.1', 'family': 'X611_A'}
-           ],
-           [
-           {'acc': 'AHB32490.1', 'family': 'X608'},
-           {'acc': 'CAI32772.1', 'family': 'X608'}
-           ],
-           [
-           {'acc': 'ACH50550.1', 'family': 'X586'}
-           ]
-           ]
+# entries = [
+#            [{'acc': '6bar', 'family': 'X571'},
+#            ],
+#            [
+#            {'acc': 'WP_011517284.1', 'family': 'X615_A'},
+#            {'acc': 'CAR61594.1', 'family': 'X615_C'}
+#            ],
+#            [
+#            {'acc': 'ACD37165.1', 'family': 'X614_B'},
+#            {'acc': 'AAM27615.1', 'family': 'X614_C'}
+#            ],
+#            [
+#            {'acc': 'CAI34254.1', 'family': 'X609'},
+#            {'acc': 'CAI34198.1', 'family': 'X609'},
+#            {'acc': 'CAI34217.1', 'family': 'X609'}
+#            ],
+#            [
+#            {'acc': 'CAI34124.1', 'family': 'X607'},
+#            ],
+#            [
+#            {'acc': 'CAI33309.1', 'family': 'X613'},
+#            {'acc': 'CAI34369.1', 'family': 'X613'},
+#            {'acc': 'AIG62747.1', 'family': 'X613'}
+#            ],
+#            [
+#            {'acc': 'BAQ00795.1', 'family': 'X605_C'},
+#            {'acc': 'BAQ01659.1', 'family': 'X605'},
+#            {'acc': 'BAQ02088.1', 'family': 'X605_B'},
+#            {'acc': 'ACA24754.1', 'family': 'X605_B'}
+#            ],
+#            [
+#            {'acc': 'AHB32215.1', 'family': 'X617_D'},
+#            {'acc': 'AHB32861.1', 'family': 'X617_D'},
+#            {'acc': 'CAI34008.1', 'family': 'X617_A'},
+#            {'acc': 'AHB32334.1', 'family': 'X617'},
+#            {'acc': 'ACH97162.1', 'family': 'X617'} 
+#            ],
+#            [
+#            {'acc': 'AHB32411.1', 'family': 'X610'}
+#            ],
+#            [
+#            {'acc': 'BAQ00653.1', 'family': 'X612'},
+#            {'acc': 'ADC54950.1', 'family': 'X612_B'},
+#            {'acc': 'AAZ85718.1', 'family': 'X612'}
+#            ],
+#            [
+#            {'acc': 'AAT77177.1', 'family': 'X606_B'},
+#            {'acc': 'CBN82200.1', 'family': 'X606_A'}
+#            ],
+#            [
+#            {'acc': 'CDF66396.1', 'family': 'X611_B'},
+#            {'acc': 'AAA97573.1', 'family': 'X611_A'},
+#            {'acc': 'AAC45857.1', 'family': 'X611_A'}
+#            ],
+#            [
+#            {'acc': 'AHB32490.1', 'family': 'X608'},
+#            {'acc': 'CAI32772.1', 'family': 'X608'}
+#            ],
+#            [
+#            {'acc': 'ACH50550.1', 'family': 'X586'}
+#            ]
+#            ]
 
 # All, with family conservation
 # entries = [
@@ -167,26 +168,40 @@ entries = [
 # ]
 
 # Selected with family conservation
-# entries = [
-#     [
-#     {'acc': '6bar', 'family': 'X571'},
-#     {'acc': 'WP_011517284.1', 'family': 'X615'},
-#     {'acc': 'AAM27615.1', 'family': 'X614'},
-#     {'acc': 'CAI34254.1', 'family': 'X609'},
-#     {'acc': 'CAI34124.1', 'family': 'X607'},
-#     {'acc': 'CAI34369.1', 'family': 'X613'},
-#     {'acc': 'BAQ02088.1', 'family': 'X605'},
-#     {'acc': 'AHB32861.1', 'family': 'X617'}
-#     ],
-#     [
-#     {'acc': 'AHB32411.1', 'family': 'X610'},
-#     {'acc': 'ADC54950.1', 'family': 'X612'},
-#     {'acc': 'CBN82200.1', 'family': 'X606'},
-#     {'acc': 'AAA97573.1', 'family': 'X611'},
-#     {'acc': 'CAI32772.1', 'family': 'X608'},
-#     {'acc': 'ACH50550.1', 'family': 'X586'}
-#     ]
-# ]
+entries = [
+    [
+    {'acc': '6bar', 'family': 'X571'},
+    {'acc': 'WP_011517284.1', 'family': 'X615'},
+    {'acc': 'AAM27615.1', 'family': 'X614'},
+    {'acc': 'CAI34254.1', 'family': 'X609'},
+    {'acc': 'CAI34124.1', 'family': 'X607'},
+    {'acc': 'CAI34369.1', 'family': 'X613'},
+    {'acc': 'BAQ02088.1', 'family': 'X605'},
+    {'acc': 'AHB32861.1', 'family': 'X617'}
+    ],
+    [
+    {'acc': 'AHB32411.1', 'family': 'X610'},
+    {'acc': 'ADC54950.1', 'family': 'X612'},
+    {'acc': 'CBN82200.1', 'family': 'X606'},
+    {'acc': 'AAA97573.1', 'family': 'X611'},
+    {'acc': 'CAI32772.1', 'family': 'X608'},
+    {'acc': 'ACH50550.1', 'family': 'X586'}
+    ]
+]
+
+# Write manual conservation file
+filename = "data/compare-architectures/conserved_manual.json"
+with open(filename) as infile:
+    conserved_dict = json.load(infile)
+
+outfilename = f"data/compare-architectures/conserved_manual.js"
+with open(outfilename, 'w') as outfile:
+    outfile.write("const conservedResidues = {\n")
+    for i in range(len(entries)):
+        for j in range(len(entries[i])):
+            entry = entries[i][j]
+            outfile.write(f"\t'{get_name(entry)}': {conserved_dict[entry['acc']]},\n")
+    outfile.write('};')
 
 # Get conserved
 for i in range(len(entries)):
