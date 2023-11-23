e708afa3cc5c626240d7dffa5dbe216d67869c99
Ida Kristine Sandford Meitil <ida.meitil@gmail.com>
2023-11-23 15:30:46 +0100

    stereochemistry barplot

--- src/ssn-clustering/analyse-superclustering/plot-stereochemistry-final-families-new.py
+++ src/ssn-clustering/analyse-superclustering/plot-stereochemistry-final-families-new.py
@@ -12,10 +12,11 @@ old2newnames = {'X617': 'GTxx4', 'X631': 'GTxx5', 'X634': 'GTxx6',
          'X613': 'GTxx7', 'X614': 'GTxx8', 'X609': 'GTxx9', 'X607': 'GTx10', 'X605': 'GTx11',
          'X610': 'GTx12', 'X612': 'GTx13', 'X606': 'GTx14', 'X611': 'GTx15', 'X608': 'GTx16', 'X633': 'GTx17'}
 for family in families:
-    new_name = old2newnames[family]
+    # new_name = old2newnames[family]
     axial_counts = len(df[(df.CAZy_family == family) & (df.axial_equatorial == 'axial')])
     equatorial_counts = len(df[(df.CAZy_family == family) & (df.axial_equatorial == 'equatorial')])
-    family2counts[new_name] = {'axial_counts': axial_counts, 'equatorial_counts': equatorial_counts}
+    # family2counts[new_name] = {'axial_counts': axial_counts, 'equatorial_counts': equatorial_counts}
+    family2counts[family] = {'axial_counts': axial_counts, 'equatorial_counts': equatorial_counts}
 print(df[(df.CAZy_family == 'X614') & (df.axial_equatorial == 'axial')])
 order = ['GTxx4', 'GTxx5', 'GTxx6', 'GTxx6', 'GTxx7', 'GTxx8', 'GTxx9', \
          'GTx10', 'GTx11', 'GTx12', 'GTx13', 'GTx14', 'GTx15', 'GTx16', 'GTx17']
