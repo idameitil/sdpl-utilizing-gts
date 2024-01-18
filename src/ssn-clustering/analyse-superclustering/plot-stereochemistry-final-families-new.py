02024c4f68210d1f3db231283df409d6eaec8b2d
Ida Kristine Sandford Meitil <ida.meitil@gmail.com>
2024-01-18 11:36:30 +0100

    changed to new family names in stereochemistry plot

--- src/ssn-clustering/analyse-superclustering/plot-stereochemistry-final-families-new.py
+++ src/ssn-clustering/analyse-superclustering/plot-stereochemistry-final-families-new.py
@@ -8,18 +8,17 @@ families = sorted(pd.unique(df.CAZy_family.astype(str)).tolist())
 families.remove('nan')
 
 family2counts = {}
-old2newnames = {'X617': 'GTxx4', 'X631': 'GTxx5', 'X634': 'GTxx6',
-         'X613': 'GTxx7', 'X614': 'GTxx8', 'X609': 'GTxx9', 'X607': 'GTx10', 'X605': 'GTx11',
-         'X610': 'GTx12', 'X612': 'GTx13', 'X606': 'GTx14', 'X611': 'GTx15', 'X608': 'GTx16', 'X633': 'GTx17'}
+old2newnames = {'GTxx4': 'GT122', 'GTxx5': 'GT123', 'GTxx6': 'GT124',
+         'GTxx7': 'GT125', 'GTxx8': 'GT126', 'GTxx9': 'GT127', 'GTx10': 'GT128', 'GTx11': 'GT129',
+         'GTx12': 'GT130', 'GTx13': 'GT131', 'GTx14': 'GT132', 'GTx15': 'GT133', 'GTx16': 'GT134', 'GTx17': 'GT135'}
 for family in families:
-    # new_name = old2newnames[family]
+    new_name = old2newnames[family]
     axial_counts = len(df[(df.CAZy_family == family) & (df.axial_equatorial == 'axial')])
     equatorial_counts = len(df[(df.CAZy_family == family) & (df.axial_equatorial == 'equatorial')])
-    # family2counts[new_name] = {'axial_counts': axial_counts, 'equatorial_counts': equatorial_counts}
-    family2counts[family] = {'axial_counts': axial_counts, 'equatorial_counts': equatorial_counts}
-print(df[(df.CAZy_family == 'X614') & (df.axial_equatorial == 'axial')])
-order = ['GTxx4', 'GTxx5', 'GTxx6', 'GTxx6', 'GTxx7', 'GTxx8', 'GTxx9', \
-         'GTx10', 'GTx11', 'GTx12', 'GTx13', 'GTx14', 'GTx15', 'GTx16', 'GTx17']
+    family2counts[new_name] = {'axial_counts': axial_counts, 'equatorial_counts': equatorial_counts}
+    # family2counts[family] = {'axial_counts': axial_counts, 'equatorial_counts': equatorial_counts}
+order = ['GT122', 'GT123', 'GT124', 'GT125', 'GT126', 'GT127', \
+         'GT128', 'GT129', 'GT130', 'GT131', 'GT132', 'GT133', 'GT134', 'GT135']
 
 # Save data
 df = pd.DataFrame.from_dict(family2counts, orient='index')
