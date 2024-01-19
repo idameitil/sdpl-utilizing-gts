76c14b33368e720853c111984225d6363813456f
Ida Kristine Sandford Meitil <ida.meitil@gmail.com>
2024-01-19 15:57:59 +0100

    csdb download

--- src/data-collection-and-preprocessing/download-csdb-images.py
+++ src/data-collection-and-preprocessing/download-csdb-images.py
@@ -3,17 +3,12 @@
 '''
 
 import os
-import re
 import sys
-import csv
 import wget
 import pandas as pd
 
-DB = "data/csdb/CSDB_slice_for_Ida.txt"
-
-os.system("ls -l {}".format(DB))
-
-images = "data/csdb/images"
+# images = "data/csdb/images"
+images = "data/csdb/images-new"
 
 def not_pd_null(value):
     return not pd.isnull(value)
@@ -64,22 +59,18 @@ def fetch_snfg_image(record_id, csdb_linear, scale=3, overwrite=False):
 	print("DONE {} {}\n".format(record_id, csdb_linear))
 
 polymerase_df = pd.read_csv("data/wzy/wzy.tsv", sep='\t', dtype={'CSDB_record_ID':'string'})
-waal_df = pd.read_csv("data/waal/waal.tsv", sep='\t', dtype={'CSDB_record_ID':'string'})
-# wanted = list(polymerase_df.CSDB_record_ID.dropna())
-# hits_df = pd.read_csv("data/wzy/blast-full-genbank/1e-15/hits-enriched.tsv", sep='\t', dtype={'CSDB_record_ID_y':'string'})
-# wanted = list(hits_df.CSDB_record_ID_y.dropna())
 
+wanted_file = "missing-sugars"
+with open(wanted_file) as f:
+    wanted = f.read().splitlines()
+print(wanted)
 # Open DB file, iterate over rows, avoiding record_ids we have already seen
 for index, row in polymerase_df.iterrows():
 
 	if not not_pd_null(row.CSDB_record_ID):
 		continue
 
-	fetch_snfg_image(row.CSDB_record_ID, row.CSDB_Linear_corrected)
-
-for index, row in waal_df.iterrows():
-
-	if not not_pd_null(row.CSDB_record_ID):
+	if not row.CSDB_record_ID in wanted:
 		continue
-
+	
 	fetch_snfg_image(row.CSDB_record_ID, row.CSDB_Linear_corrected)
