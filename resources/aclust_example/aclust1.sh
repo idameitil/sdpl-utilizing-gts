dfaebc98b14ea81f60011f37a8a64730ddedd24c
Ida Meitil <idameitil@MacBook-Air-tilhrende-Ida.local>
2025-03-12 10:01:06 +0100

    cleanup

--- resources/aclust_example/aclust1.sh
+++
@@ -1,9 +0,0 @@
-cd $1
-
-sed 's/ .*$//' $2 | chunkfasta -c 10 -- > chunkfasta.out
-
-mkdir run
-
-/work3/idamei/bin/aclust_example/runalignfastas.py > submit.sh
-
-sh submit.sh
--- resources/aclust_example/aclust2.sh
+++
@@ -1,5 +0,0 @@
-cd $1
-
-/work3/idamei/bin/aclust_example/gendmx
-
-bsub < /work3/idamei/bin/aclust_example/reembed_bsub.sh
